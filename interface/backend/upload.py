4a467e0031807aa890e8dc4bc1e5bc6d4f8e3428
Minho Menezes <minhotmog@gmail.com>
2018-12-04 00:40:03 -0300

    Fixing the videoplayer bug in the statistics page

--- interface/backend/upload.py
+++ interface/backend/upload.py
@@ -3,15 +3,16 @@ from flask import flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
 from . import util
+from ... import settings
 from ...utils.counter import counter 
 
 def file(upload_folder):
 	'''código retirado de http://flask.pocoo.org/docs/1.0/patterns/fileuploads/'''
 	if request.method == 'POST':
-		# check if the post request has the file part
 		if 'file' not in request.files:
 			flash('No file part')
 			return redirect(request.url)
+		
 		file = request.files['file']
 		
 		if file.filename == '':
@@ -20,10 +21,16 @@ def file(upload_folder):
 		
 		if file and util.allowed_file_format(file.filename):
 			filename = "video" + secure_filename(file.filename)[-4:]
-			file.save(os.path.join(upload_folder, filename))
+			numVideos = len(next(os.walk(upload_folder))[1])
 
-			counter(upload_folder, filename)
+			folder = "{0}{1:02d}{2}".format(upload_folder, numVideos, settings.slash)
+			folder_relative = "static/data/{0:02d}/".format(numVideos)
 
-			return render_template('statistics.html', videopath=os.path.join(upload_folder, "output.mp4"))
+			os.makedirs(folder)
+			file.save(folder + filename)
+
+			counter(folder, filename)
+
+			return render_template('statistics.html', videopath=folder_relative)
 
 	return render_template('home.html')
